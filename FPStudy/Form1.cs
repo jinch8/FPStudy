9a1a8684ef9d212929de84c6c3893fb1085a822b
유 진충 <lzz@cadwin.lan>
2017-12-11 11:33:51 +0900

    test

---
+++ FPStudy/Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using FlyPig.Util;
+using System.IO;
+using System.Diagnostics;
+
+namespace FPStudy
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            IniHelper iniHelper = new IniHelper(Path.Combine(Environment.CurrentDirectory, "test.ini"));
+            iniHelper.WriteValue("sec", "key1", "value1");
+            MessageBox.Show("ok");
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            Process.Start(Environment.CurrentDirectory);
+        }
+    }
+}
